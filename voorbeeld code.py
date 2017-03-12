486d730111d4afe0bae40a895c3553a1fc8fae25
rm93 <snorkelen_123fan@live.nl>
2017-03-12 14:58:17 +0100

    Meeloopdag

---
+++ voorbeeld code.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 14:34:27 2017
+
+@author: Rick
+"""
+#Vraag om de naam en zet dit in de variabele naam
+naam = input("Wat is jouw naam? ")
+
+#Vraag om de leeftijd en zet dit in de variabele leeftijd
+leeftijd = int(input("Hoe oud ben je? "))
+
+#Reken uit hoe oud je bent over 10 jaar
+O10 = (leeftijd+10)
+
+#Print de naam en zijn leeftijd over 10 jaar in een zin
+print(naam,"over 10 jaar ben je",O10,"jaar oud")
